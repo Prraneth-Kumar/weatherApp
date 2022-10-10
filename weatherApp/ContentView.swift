9bd93ad14be734b8d59dd6dc599a910e01edda76
Prraneth-Kumar <prranethkumar22@gmail.com>
2022-10-10 19:29:18 +0530

    commit first

--- weatherApp/ContentView.swift
+++ weatherApp/ContentView.swift
@@ -14,52 +14,67 @@ struct ContentView: View {
     
     @State private var cityName = ""
     @State private var page2 = false
-    
+
     @State private var name =  ""
     @State private var clouds = 0
     @State private var humidity = 0
     @State private var speed  = 0.0
     @State private var pressure  = 0
     @State private var temp = 0.0
+    
     var body: some View {
-        NavigationView{
+        
+        NavigationView {
             
-            VStack {
+            ZStack {
                 
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                TextField("Enter City or State", text: $cityName)
-                Button("Tapped"){
-                    APICall()
-                    page2.toggle()
+                LinearGradient(gradient: Gradient(colors: [.red, .orange]), startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                
+                VStack{
+                    // Spacer()
                     
-                }
-//                NavigationLink(destination:  NewPage( name: name,clouds: clouds,humidity: humidity,speed: speed,pressure: pressure, temp: temp),isActive: $page2 ){
-//                }
-                
+                    Text("Enter City Name For Weather")
+                    TextField("Enter City or State", text: $cityName)
+                    Button("Search"){
+                        guard cityName != "" else{
+                            return
+                        }
+                        APICall()
+                        page2.toggle()
+                    }.fontWeight(.semibold)
+                    //NavigationLink(destination:  NewPage( name: name,clouds: clouds,humidity: humidity,speed: speed,pressure: pressure, temp: temp),isActive: $page2 ){
+                    //}
+                    //   Spacer()
+                    
+                }.padding()
             }
-            .padding()
-            //            .sheet(isPresented: $page2){
-            //                NewPage( name: name,clouds: clouds,humidity: humidity,speed: speed,pressure: pressure, temp: temp)
-            //            }
+            
+            
+            .sheet(isPresented: $page2){
+                NewPage( name: name,clouds: clouds,humidity: humidity,speed: speed,pressure: pressure, temp: temp)
+            }
         }
+        
     }
-
     func APICall(){
         let urlLogin = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&APPID=c763b32df4592228bd42fae731558678&units=metric"
         Alamofire.request(urlLogin, method:.get).responseJSON{
             response in
-            print(response)
+            // print(response)
             let json = try! JSON(data: response.data!)
+            print(json.count)
+            
+            guard json["cod"] != "404" else{
+                //weather()
+                return
+            }
             name = json["name"].string!
             clouds = json["clouds"]["all"].int!
             humidity = json["main"]["humidity"].int!
             speed = json["wind"]["speed"].doubleValue
             pressure = json["main"]["pressure"].int!
             temp = json["main"]["temp"].doubleValue
-            //weather()
         }
     }
 }
@@ -72,7 +87,6 @@ struct NewPage: View{
     var pressure : Int
     var temp: Double
     //let lista = (name,clouds, humidity,speed,pressure,temp)
-    
     var body: some View {
         VStack{
             Button("Okay"){
@@ -80,15 +94,26 @@ struct NewPage: View{
                 print(name,clouds,humidity,speed,pressure,temp)
             }
             List{
-                HStack{
-                    Image("cloud")
-                        .resizable()
-                        .scaledToFill()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(maxWidth: 100, maxHeight: 100)
-                    VStack{
-                        Text(name).fontWeight(.bold)
-                        Text("\(temp, specifier: "%.2f")°C").fontWeight(.bold)
+                if name == ""{
+                    Text("Enter City Name Correctly")
+                }
+                else{
+                    HStack {
+                        VStack{
+                            Text(name).fontWeight(.bold)
+                            Image("cloud")
+                                .resizable()
+                                .scaledToFill()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(maxWidth: 100, maxHeight: 100)
+                        }
+                        VStack(alignment: .leading){
+                            Text("Temp: \(temp, specifier: "%.2f")°C").fontWeight(.bold)
+                            Text("Clouds: \(clouds)%").fontWeight(.bold)
+                            Text("Humidity: \(humidity)%").fontWeight(.bold)
+                            Text("Speed: \(speed, specifier: "%.2f")m/s").fontWeight(.bold)
+                            Text("Pressure: \(pressure)hpa").fontWeight(.bold)
+                        }
                     }
                 }
             }.listStyle(.grouped)
@@ -96,24 +121,6 @@ struct NewPage: View{
     }
 }
 
-//
-//func weather(){
-//    print(lat, lon)
-//
-//    let urlWeather = "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=c763b32df4592228bd42fae731558678"
-//
-//
-//    Alamofire.request(urlWeather, method:.get).responseJSON{
-//        response in
-//        print(response)
-//        //        let json = try! JSON(data: response.data!)
-//        //        let location = json["coord"]
-//        //        lat = location["lat"].doubleValue
-//        //        lon = location["lon"].doubleValue
-//        //        print(lat, lon)
-//    }
-//}
-    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
